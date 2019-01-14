abacb750ad41d6f9f0a30d95d14d42e95ca2be5e
Piyush Mishra <piyushmishra1999@rediffmail.com>
2019-01-14 13:09:36 +0530

    beta update ui

--- js/scrypt.js
+++ js/scrypt.js
@@ -96,5 +96,91 @@ $(function () {
 });
 
 function formFilled(){
-  alert("Thank you for your input the brochure will be sent to your Email-Id")
+  var val = document.getElementById('email');
+  if(val.value == ""){
+    val.setCustomValidity("Enter a Valid Email");
+  }
+  else{
+    alert("Thank you for your input the brochure will be sent to your Email-Id")
+  }
+  
 }
+
+
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+jQuery(function($) {
+
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+
+    // Check all animatables and animate them if necessary
+    $animatables.each(function(i) {
+       var $animatable = $(this);
+      if (($animatable.offset().top + $animatable.height() - 20) < offset ) {
+        $animatable.removeClass('animatable').addClass('animated');
+      }
+     
+    });
+
+  };
+
+  // Hook doAnimations on scroll, and trigger a scroll
+  $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= ($(document).height() - $(window).height()) ) {
+        $('.signature:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.signature').stop(true, true).fadeOut();
+    }
+});
+
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 150) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+$("#learnmore").on("click",function(){
+        $('html,body').animate({
+        scrollTop: $("#section2").offset().top-100},
+        'slow');
+})
